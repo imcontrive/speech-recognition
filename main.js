571ed3fad1ebaeb187d71815a9348b96761ed657
shubh <psubham94@gmail.com>
2019-07-20 10:45:17 +0530

    modified:   index.html modified:   main.js

--- main.js
+++ main.js
@@ -1,3 +1,31 @@
 
 
-// window.SpeechRecognition = window.SpeechRecognition ||window.webkitSpeechRecognition 
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+			
+
+const recognition = new SpeechRecognition();
+recognition.interimResults = true;
+recognition.lang = 'en-US';
+
+var p = document.createElement('p');
+const content = document.querySelector('.content');
+content.appendChild(p);
+
+recognition.addEventListener('result', e => {
+const transcript = Array.from(e.results)
+  .map(result => result[0])
+  .map(result => result.transcript)
+  .join('');
+
+  const poopScript = transcript.replace(/poop|poo|shit|dump/gi, '💩');
+  p.textContent = poopScript;
+
+  if (e.results[0].isFinal) {
+    p = document.createElement('p');
+    words.appendChild(p);
+  }
+});
+
+recognition.addEventListener('end', recognition.start);
+
+recognition.start();
